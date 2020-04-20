677278f839688244398cfd84d926503e44c4ed2d
Finn Terdal <fterdal@gmail.com>
2020-04-20 00:24:38 -0500

    gimme some rect angels

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import "./App.css"
 function App() {
   return (
     <div className="App">
-      <svg height="120" width="720">
+      <svg height="240" width="720">
         <g>
           <circle cx="40" cy="60" r="10"></circle>
           <circle cx="80" cy="60" r="10"></circle>
           <circle cx="120" cy="60" r="10"></circle>
+        </g>
+        <g>
+          <rect x="40" y="120" height="20" width="20"></rect>
+          <rect x="80" y="120" height="20" width="20"></rect>
+          <rect x="120" y="120" height="20" width="20"></rect>
         </g>
       </svg>
     </div>
