761ceea7748fb281616f07094daaad7aab41fb9b
Finn Terdal <fterdal@gmail.com>
2020-04-28 21:46:25 -0500

    got some squares

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import React from "react"
-import "./App.css"
+import React from 'react';
+import './App.css';
+
+const voteTallies = {
+  apple: 18,
+  orange: 12,
+  pear: 10,
+  banana: 9,
+  mango: 6,
+};
 
 function App() {
   return (
     <div className="App">
-      <svg height="240" width="720">
-        <g>
-          <circle cx="40" cy="60" r="10"></circle>
-          <circle cx="80" cy="60" r="10"></circle>
-          <circle cx="120" cy="60" r="10"></circle>
-        </g>
-        <g>
-          <rect x="40" y="120" height="20" width="20"></rect>
-          <rect x="80" y="120" height="20" width="20"></rect>
-          <rect x="120" y="120" height="20" width="20"></rect>
-        </g>
+      <svg style={{ border: '1px solid black' }} height="240" width="720">
+        {Object.entries(voteTallies).map(([cand, tally], candIdx) => {
+          // console.log('cand', cand);
+          // console.log('tally', tally);
+          return (
+            <g key={cand}>
+              {new Array(tally).fill('s').map((_, tallyIdx) => (
+                <rect
+                  key={tallyIdx}
+                  x={tallyIdx * 25}
+                  y={candIdx * 25}
+                  height="20"
+                  width="20"
+                ></rect>
+              ))}
+            </g>
+          );
+        })}
       </svg>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
